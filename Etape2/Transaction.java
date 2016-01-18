22aa3f376dad54540fee1f03c2a3e7befc72eed3
lallemj <jules.lallemand@orange.fr>
2016-01-18 21:22:56 +0100

    debut modif Transaction

--- Etape2/Transaction.java
+++ Etape2/Transaction.java
@@ -1,25 +1,46 @@
+import java.util.HashMap;
+
 public class Transaction {
+
+	private static Transaction transaction;
+	
+    private boolean active;
+	
+	private HashMap<Integer,Object> objetsaccedes;
 	
 	public Transaction() {
+		transaction = this;
+		this.active = false;
+		this.objetsaccedes = new HashMap<Integer,Object>();
 	}	
 	
 	public static Transaction getCurrentTransaction() {
+		return transaction;
 	}
 
 	// indique si l'appelant est en mode transactionnel
 	public boolean isActive() {
+		return this.active;
 	}
 	
 	// demarre une transaction (passe en mode transactionnel)
 	public void start() {
+		this.active = true;
 	}
 	
 	// termine une transaction et passe en mode non transactionnel
-	public boolean commit(){
+	public boolean commit() {
+		this.objetsaccedes.clear();
+		this.active = false;
 	}
 		
 	// abandonne et annule une transaction (et passe en mode non transactionnel)
-	public void abort(){
+	public void abort() {
+		for(Integer i : this.objetsaccedes.keySet()) {
+            Client.sharedObjects.get(i).obj = this.objetsaccedes.get(i);
+        }
+		this.objetsaccedes.clear();
+		this.active = false;
 	}
 	
 }
