554e68939a9167bc787dcd88651657bac9129f83
lallemj <jules.lallemand@orange.fr>
2016-01-21 17:54:32 +0100

    ajout SharedObject etape3

---
+++ Etape3/SharedObject.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SharedObject implements Serializable, SharedObject_itf {
+
+	private int id;
+
+	public Object obj;
+
+	private static final int NL = 0;
+	private static final int RLC = 1;
+	private static final int WLC = 2;
+	private static final int RLT = 3;
+	private static final int WLT = 4;
+	private static final int RLT_WLC = 5;
+	private int lock;
+
+	private Client client;
+
+	public SharedObject (Object o, int id) {
+		this.obj = o;
+		this.id = id;
+		this.lock = NL;
+	}
+
+	public int getID() {
+		return this.id;
+	}
+
+	public int getLockType() {
+		return this.lock;
+	}
+
+	// invoked by the user program on the client node
+	public void lock_read() {
+		switch(this.lock) {
+		case NL :
+			this.lock = RLT;
+			this.obj = Client.lock_read(this.id);	
+		break;
+		case RLC :
+			this.lock = RLT;
+		break;
+		case WLC :
+			this.lock = RLT_WLC;
+		break;
+		}
+	}
+
+	// invoked by the user program on the client node
+	public void lock_write() {
+		switch(this.lock) {
+		case NL :
+			this.lock = WLT;
+			this.obj = Client.lock_write(this.id);
+		break;
+		case RLC :
+			this.lock = WLT;
+			this.obj = Client.lock_write(this.id);
+		break;
+		case WLC :
+			this.lock = WLT;
+		break;
+		}
+	}
+
+	// invoked by the user program on the client node
+	public synchronized void unlock() {
+		switch(this.lock) {
+		case RLT :
+			this.lock = RLC;
+		break;
+		case WLT :
+			this.lock = WLC;
+		break;
+		case RLT_WLC :
+			this.lock = WLC;
+		break;
+		}
+		this.notifyAll();
+	}
+
+
+	// callback invoked remotely by the server
+	public synchronized Object reduce_lock() {
+		while(lock == WLT || lock == RLT_WLC) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        lock = RLC;
+		return this.obj;
+	}
+
+	// callback invoked remotely by the server
+	public synchronized void invalidate_reader() {
+		while(lock == RLT) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+		lock = NL;
+	}
+
+	public synchronized Object invalidate_writer() {
+		while(lock == RLT_WLC || lock == WLT) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        lock = NL;
+		return this.obj;
+	}
+}
